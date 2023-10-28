41923c80e4b186fd7fbcb4d203e4928b7aac7f76
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2023-10-28 20:00:49 +0100

    moving global extension functions

--- src/plotting.jl
+++ src/plotting.jl
@@ -9,3 +9,21 @@ function plot_vertices end
 function plot_vertex! end
 function plot_connectome end
 function plot_connectome! end
+
+const meshpath = artifact"DKTMeshes"
+mni_cortex() = joinpath(meshpath, "meshes/cortex/connectome-cortex.obj")
+fs_cortex() =  joinpath(meshpath, "meshes/cortex/fs-cortex.obj")
+rh_cortex() =  joinpath(meshpath, "meshes/cortex/rh-cortex.obj")
+lh_cortex() =  joinpath(meshpath, "meshes/cortex/lh-cortex.obj")
+
+function set_fig(;resolution::Tuple{Int64, Int64}=(1600,900), view=:front)
+    f = Figure(resolution = resolution)
+    ax = Axis3(f[1,1], aspect = :data, azimuth = View[view]pi, elevation=0.0pi)
+    hidedecorations!(ax)
+    hidespines!(ax)
+    f
+end
+
+RegionDict = Dict(zip([:left, :right, :all, :connectome], [lh_cortex(), rh_cortex(), fs_cortex(), mni_cortex()]))
+
+View = Dict(zip([:right, :front, :left, :back], [0.0, 0.5, 1.0, 1.5]))
